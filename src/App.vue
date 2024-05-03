96e8ed1ecefa13cf76bb37db0b975713d5439fdd
Nathan Furnal <nathan@adimian.com>
2024-05-03 18:56:32 +0200

    base project for vue calculator

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header class="m-2">
+  <h2>Use the calculator for intensive maths (⌐⊙_⊙)</h2>
+</header>    
+<main class="center m-2">
+  <section class="center m-2 text-center">
+    <div class="m-2 reader nes-container">
+      <p></p>
+    </div>
+    <div class="buttons nes-container">
+    </div>
+  </section>
+</main>
+  <footer>
+  </footer>
+</template>
